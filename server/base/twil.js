f2271536762301547f0b53227454d441301dfdc2
Jane <janecakemaster@gmail.com>
2015-08-04 19:49:48 -0400

    secure webhook

--- server/base/twil.js
+++ server/base/twil.js
@@ -18,10 +18,25 @@ function twil() {
     };
 
     this.receiveLine = function(request, reply) {
-        var resp = new Twilio.TwimlResponse();
-        resp.message('this is a reply');
-        reply(resp.toString()).type('text/xml');
+        if (fromTwilio(request)) {
+            var resp = new Twilio.TwimlResponse();
+            resp.message('this is a reply');
+            reply(resp.toString()).type('text/xml');
+        }
+        else {
+            reply.view('404', {
+                title: 'bad shit dude'
+            }).code(404);
+        }
     };
+}
+
+function fromTwilio(request) {
+    var sig = request.headers['x-twilio-signature'],
+        url = request.url,
+        body = request.payload || {};
+
+    return Twilio.validateRequest(authToken, sig, url, body);
 }
 
 module.exports = twil;
