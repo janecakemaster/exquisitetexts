ce04740aed897a50f2ca3a90b2b3d8f4c98a0ebc
Jane <janecakemaster@gmail.com>
2015-12-01 01:31:00 -0500

    use gulp to do js stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,35 @@ var sass = require('gulp-sass');
 var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sassLint = require('gulp-sass-lint');
-
-// gulp.task('sass', function () {
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./css'));
-// });
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
   gulp.src('src/sass/**/*.scss')
-    .pipe(sassLint())
-    .pipe(sassLint.format())
-    .pipe(sass({
-      includePaths: require('node-normalize-scss').includePaths
-    }))
-    .pipe(minifycss())
-    .pipe(rename({
-      extname: '.min.css'
-    }))
-    .pipe(gulp.dest('public/stylesheets/'));
+  .pipe(sassLint())
+  .pipe(sassLint.format())
+  .pipe(sass({
+    includePaths: require('node-normalize-scss').includePaths
+  }))
+  .pipe(minifycss())
+  .pipe(rename({
+    extname: '.min.css'
+  }))
+  .pipe(gulp.dest('public/stylesheets/'));
 });
 
+gulp.task('js', function() {
+  gulp.src('src/js/**/*.js')
+  .pipe(concat('app.js'))
+  .pipe(uglify())
+  .pipe(rename({
+      extname: '.min.js'
+  }))
+  .pipe(gulp.dest('public/javascripts/'));
+});
 
 gulp.task('sass:watch', function () {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', function () {
-}); 
-
+gulp.task('default', ['sass']);
