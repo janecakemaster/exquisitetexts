3b59780022acb7ec5a7977d94ff2f98e91ac5da0
Jane <janecakemaster@gmail.com>
2015-12-01 08:01:01 -0500

    break out routes

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var mainRoutes = require('./routes/index');
+var poemRoutes = require('./routes/poems');
+var twilioRoutes = require('./routes/twilio');
 
 var app = express();
 
@@ -13,15 +15,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/', mainRoutes);
+app.use('/poem', poemRoutes);
+app.use('/twilio', twilioRoutes);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,10 @@
 
 var express = require('express');
 var router = express.Router();
-var Firebase = require('firebase');
-var poemsRef = new Firebase('https://exquisitehues.firebaseio.com/poems');
 
 ////////////
 // Routes //
 ////////////
-
-router.get('/poem/:id', function(req, res, next) {
-  var poemid = req.params.id,
-  poemRef = poemsRef.child(poemid);
-
-  poemRef.once('value', function(snapshot) {
-    var poem = snapshot.val();
-
-    if (poem && poem.lines) {
-      onRetrieveSuccess(req, res, next, { poem: poem, id: poemid });
-    }
-
-    else {
-      next();
-    }
-  });
-});
 
 router.get('/about', function(req, res) {
   res.render('about', { title: 'about' });
@@ -35,65 +16,5 @@ router.get('/about', function(req, res) {
 router.get('/', function(req, res) {
   res.render('index');
 });
-
-/////////////
-// Helpers //
-/////////////
-
-function onRetrieveSuccess(req, res, next, data) {
-  var prevId = parseInt(data.id, 10) - 1,
-      prevRef = poemsRef.child(prevId);
-
-  prevRef.once('value', function(snapshot) {
-    var poem = snapshot.val();
-
-    if (poem) {
-      data.prevPoemId = prevId;
-      checkNext(req, res, next, data);
-    }
-    else {
-      renderPoem(req, res, next, data);
-    }
-  });
-}
-
-function checkNext(req, res, next, data) {
-  var nextId = parseInt(data.id, 10) + 1,
-      nextRef = poemsRef.child(nextId);
-
-  nextRef.once('value', function(snapshot) {
-    var poem = snapshot.val();
-
-    if (poem) {
-      data.nextPoemId = nextId;
-      renderPoem(req, res, next, data);
-    }
-    else {
-      renderPoem(req, res, next, data);
-    }
-  });
-}
-
-function renderPoem(req, res, next, data) {
-  res.render('poem', {
-    title: 'poem ' + data.id,
-    poemid: data.id,
-    poem: data.poem.lines,
-    time: renderDate(new Date(data.poem.timestamp)),
-    prevPoem: data.prevPoemId,
-    nextPoem: data.nextPoemId
-  });
-}
-
-function renderDate(date) {
-  var year = date.getUTCFullYear(),
-      month = date.getMonth() + 1,
-      day = date.getDate(),
-      hour = String("00" + date.getHours()).slice(-2),
-      minutes = String("00" + date.getMinutes()).slice(-2);
-
-  return month + '/' + day + '/' + year + ' ' + hour + ':' +
-  minutes;
-}
 
 module.exports = router;
