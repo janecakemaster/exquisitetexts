a8d769549129e5ff43e908af44f1d23b3d4a8adb
Jane <janecakemaster@gmail.com>
2015-12-01 02:07:03 -0500

    use includes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var titleBase = 'exquisite texts',
+	titleDiv = ' - ';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: titleBase });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: titleBase + titleDiv + 'about' });
 });
 
 module.exports = router;
