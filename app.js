50ba49d1ac7a0cc01267092411ed958c657ef8c6
Jane <janecakemaster@gmail.com>
2015-12-01 04:48:45 -0500

    display poems

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -51,7 +50,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
+    message: 'Nothing to see here...',
     error: {}
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,43 @@
+var Firebase = require('firebase');
 var express = require('express');
 var router = express.Router();
 
-var titleBase = 'exquisite texts';
+var titleBase = 'exquisite texts',
+poemsRef = new Firebase('https://exquisitehues.firebaseio.com/poems');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/poem/:id', function(req, res, next) {
+  var poemid = req.params.id,
+      poemRef = poemsRef.child(poemid);
+
+  poemRef.once('value', function(snapshot) {
+    var poem = snapshot.val();
+
+    if (poem && poem.lines) {
+      res.render('poem', {
+        title: 'poem ' + poemid,
+        poemid: poemid,
+        poem: poem.lines,
+        time: renderDate(new Date(poem.timestamp))
+      })
+    }
+    else {
+      next();
+    }
+  });
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res) {
   res.render('about', { title: 'about' });
 });
+
+function renderDate(date) {
+    var year = date.getUTCFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate(),
+        hour = String("00" + date.getHours()).slice(-2),
+        minutes = String("00" + date.getMinutes()).slice(-2);
+    return month + '/' + day + '/' + year + ' ' + hour + ':' +
+        minutes;
+}
 
 module.exports = router;
